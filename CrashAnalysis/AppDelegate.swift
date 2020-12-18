a512345d3158c067547823dc76b488c475469f71
JyHu <auu.aug@gmail.com>
2020-12-18 14:40:48 +0800

    Initial Commit

---
+++ CrashAnalysis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CrashAnalysis
+//
+//  Created by 胡金友 on 2020/12/18.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
